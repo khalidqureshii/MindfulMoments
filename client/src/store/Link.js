518e2afc1c5ab38ee0dcc52c3668e35766f6259a
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2025-04-25 18:34:49 +0530

    Update Link.js

--- client/src/store/Link.js
+++ client/src/store/Link.js
@@ -1,5 +1,5 @@
 // const LINK = "https://journalling.onrender.com/".toString();
-const  LINK = "http://localhost:5000/".toString();
-// const LINK = "https://mindful-moments-zeta.vercel.app/".toString();
+// const  LINK = "http://localhost:5000/".toString();
+const LINK = "https://mindful-moments-zeta.vercel.app/".toString();
 
 export default LINK;
